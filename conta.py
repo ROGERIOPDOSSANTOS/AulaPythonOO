5800814faf2dd638f4825b06b09d8e6f3a0ae5ad
ROGERIO P DOS SANTOS <rogerio.dosantos@ifpr.edu.br>
2021-10-31 10:32:56 -0300

    1

---
+++ conta.py
@@ -0,0 +1,58 @@
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def pega_saldo(self):
+        return self.__saldo
+
+    def devolve_titular(self):
+        return self.__titular
+
+    def retorna_limite(self):
+        return self.__limite
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_limite(self):
+        return self.__limite
+
+    def  set_limite(self, limite):
+        self.__limite = limite
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
