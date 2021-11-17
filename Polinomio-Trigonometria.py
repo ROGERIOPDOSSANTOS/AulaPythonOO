67be7fb8ed26fea0028023540fb31944516020e7
user.email <rogerio.dosantos@ifpr.edu.br>
2021-11-16 21:35:18 -0300

    1

---
+++ Polinomio-Trigonometria.py
@@ -0,0 +1,13 @@
+
+########### Polinomio - console
+
+from sympy.solvers import solve
+from sympy import Symbol
+
+x = Symbol ('x')
+solve(x**2 - 3*x - 10 , x)
+
+########### Trigonometria - console - Hiparco
+
+
+
