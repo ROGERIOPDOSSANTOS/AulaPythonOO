8796da6eae0984ae70d42a56426b8a900365ab92
user.email <rogerio.dosantos@ifpr.edu.br>
2021-12-24 17:34:21 -0300

    Update aula1.py

--- aula1.py
+++
@@ -1,9 +0,0 @@
-import pyteste
-from finefibo.fibo import fibo.numbers
-
-def test_fibo_number_values():
-    assert fibo_number(0) == 0
-    assert fibo_number(1) == 1
-    assert fibo_number(2) == 2
-    assert fibo_number(3) == 3
-    assert fibo_number(4) == 4
