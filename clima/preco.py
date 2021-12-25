6cd3f93e3bf0eeb1316635a0a50fe78a4d991c6d
user.email <rogerio.dosantos@ifpr.edu.br>
2021-12-25 01:04:50 -0300

    7

--- clima/preco.py
+++
@@ -1,9 +0,0 @@
-
-def aumento(valor, porcentagem):
-    r = valor + (valor * (porcentagem  / 100))
-    return r
-
-def reducao(valor, porcentagem):
-    r = valor - (valor * (porcentagem  / 100))
-    return r
-
