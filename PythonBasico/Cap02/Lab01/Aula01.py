d99af25f40e7527bb1660439c311854e7f2a595b
user.email <rogerio.dosantos@ifpr.edu.br>
2022-03-17 09:45:24 -0300

    1

---
+++ PythonBasico/Cap02/Lab01/Aula01.py
@@ -0,0 +1,20 @@
+
+print("ALO MUNDO")
+print(2+2)
+print(5-2)
+print(2*2)
+print(10/2)
+print(2**2)
+print(10%3)
+print("   *******       ********")
+print("            **     ")
+print("      00            00  ")
+print("            0 0   ")
+print("      ***         ***")
+print("          ***   ***")
+print("           < > !!!&&&&&&&&&&$$$$$$$$$@@@@@@@@@#######   **")
+
+
+
+
+
