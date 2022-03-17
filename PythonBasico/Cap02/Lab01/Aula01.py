8066175cb0bda7dc4157be7e7b0b01cf4595357a
user.email <rogerio.dosantos@ifpr.edu.br>
2022-03-17 11:01:42 -0300

    Aula dia 17-03

--- PythonBasico/Cap02/Lab01/Aula01.py
+++ PythonBasico/Cap02/Lab01/Aula01.py
@@ -1,18 +1,28 @@
-
+#aprendendo a escrever na tela
 print("ALO MUNDO")
+"""
+Escrever acima de duas linhas uso aspas tripas
+eu gosto do melhor prof do mundo
+prof Rogerio
+"""
+#Operação matematica
 print(2+2)
 print(5-2)
 print(2*2)
 print(10/2)
 print(2**2)
 print(10%3)
+#Desenhando na tela
 print("   *******       ********")
 print("            **     ")
 print("      00            00  ")
 print("            0 0   ")
 print("      ***         ***")
 print("          ***   ***")
-print("           < > !!!&&&&&&&&&&$$$$$$$$$@@@@@@@@@#######   **")
+print(" < > !!!&&&&$$$@@@@@@#####   **")
+#Resolvendo problema de Nota
+
+#Resolvendo problema de porcentagem
 
 
 
